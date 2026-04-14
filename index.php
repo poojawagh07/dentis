33c027ddc77f088a9a7a36f779117018ecae9ff5
Pooja Wagh <118878218+poojawagh07@users.noreply.github.com>
2026-04-14 17:48:33 +0530

    initial update

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php include './includes/header.php'; ?>
+
+
+<section class="hero-section">
+  <div class="container hero-container">
+
+    <!-- LEFT CONTENT -->
+    <div class="hero-content">
+      <h1>
+        Transforming Smiles with <br>
+        Precision <span>& Care</span>
+      </h1>
+
+      <p>
+        Experience advanced dental treatments designed for comfort,
+        confidence, and long-term oral health.
+      </p>
+
+      <div class="hero-buttons">
+        <a href="#" class="btn primary">Book an appointment</a>
+        <a href="#" class="btn outline">📞 Call Now</a>
+      </div>
+    </div>
+
+    <!-- RIGHT IMAGE -->
+    <div class="hero-image">
+      <img src="assets/images/clinic.jpg" alt="Dental Clinic">
+    </div>
+
+  </div>
+</section>
+
+<section class="cta-section">
+  <div class="cta-container">
+
+    <!-- LEFT -->
+    <div class="cta-box gradient-box">
+      <!-- <p class="small-text">Our Team</p> -->
+      <h3>About Clinic</h3>
+      <a href="#" class="cta-btn">Our Team</a>
+    </div>
+
+    <!-- CENTER -->
+    <div class="cta-box center-box">
+      <h3>Book An Appointment</h3>
+      <a href="#" class="cta-btn ">Connect with Us</a>
+    </div>
+
+    <!-- RIGHT -->
+    <div class="cta-box gradient-box">
+      <h3>Treatment And Services</h3>
+      <a href="#" class="cta-btn">Read More</a>
+    </div>
+
+  </div>
+</section>
+<script src="assets/js/script.js"></script>
